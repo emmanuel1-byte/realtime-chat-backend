ec09b2a4af361e37602111072d480f8767e56d57
emmanuel1-byte <emmnauelhilary9@gmail.com>
2024-09-17 10:56:12 +0100

    added the swagger doc

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7000;
 import { createServer } from "node:http";
 import cookieParser from "cookie-parser";
 import cors from "cors";
